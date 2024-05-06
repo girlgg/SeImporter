06b514f8f0790980a2ab5281a53d4455fef45be7
misterwu886 <131601220+misterwu886@users.noreply.github.com>
2024-05-06 22:34:39 +0800

    Update SeAnimOptions.h
    3

--- Source/SeImporter/Private/Widgets/SeAnimOptions.h
+++ Source/SeImporter/Private/Widgets/SeAnimOptions.h
@@ -18,5 +18,24 @@ public:
 	UPROPERTY(EditAnywhere, Category = "Import Settings")
 	TSoftObjectPtr<USkeleton> Skeleton;
 
+	UPROPERTY(EditAnywhere, Category = "Anim Settings", meta = (ToolTip = "Override the animation type if enabled"))
+	bool bOverrideAnimType;
+	
+	UPROPERTY(EditAnywhere, Category = "Anim Settings", meta = (ToolTip = "Specify the type of animation if override is enabled", EditCondition = "bOverrideAnimType"))
+	ESeAnimAnimationType AnimType;
+
+	UPROPERTY(EditAnywhere, Category = "Anim Settings", meta = (ToolTip = "Use Cure To Save Animtion"))
+	bool bUseCurveeSave=false;
+
+	
+	UPROPERTY(EditAnywhere, Category = "Anim Settings", meta = (ToolTip = "Override the refpose if enabled"))
+	bool bOverrideRefpose;
+	
+	UPROPERTY(EditAnywhere, Category = "Anim Settings", meta = (ToolTip = "Refpose Sequence",EditCondition = "bOverrideRefpose"))
+	TObjectPtr<UAnimSequence>  RefposeSequence;
+
+	UPROPERTY(EditAnywhere, Category = "Anim Settings", meta = (ToolTip = "Refpose time",EditCondition = "bOverrideRefpose"))
+	int PoseTime=0;
+
 	bool bInitialized{false};
 };
